de1d2ee3091b0572426e6436724c117a8635d7fd
icebear <icebearliangliang@163.com>
2019-04-14 16:27:23 +0800

    add read file

--- product.py
+++ product.py
@@ -1,4 +1,17 @@
+# 读取档案
 products = []
+with open('products.csv', 'r', encoding = 'utf-8') as f:
+	for line in f:
+		if '商品, 价格' in line:
+			continue # 继续（跳到下一回）
+		name, price = line.strip().split(',') # 做这步的目的是让清单的格式变回和下面的一样，这样后面再做处理就不会出错
+		products.append([name,price])
+
+print(products)
+
+
+
+
 while True:
 	name = input('请输入商品名称：')
 	if name == 'q':
@@ -9,8 +22,8 @@ while True:
 	p.append(name)
 	p.append(price)
 	products.append(p)
-	# p = [name, price]
-	# products.append([name, price])
+	#p = [name, price]
+	#products.append([name, price])
 print(products)
 
 
