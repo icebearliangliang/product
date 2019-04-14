4cd10972bc33a537d95346fa92f7bfef4661150e
icebear <icebearliangliang@163.com>
2019-04-14 15:39:21 +0800

    add utf-8

--- product.py
+++ product.py
@@ -4,6 +4,7 @@ while True:
 	if name == 'q':
 		break
 	price = input('请输入商品价格：')
+	price = int(price)
 	p = []
 	p.append(name)
 	p.append(price)
@@ -17,7 +18,8 @@ for p in products:
 	print(p[0], '的价格是', p[1])
 
 
-with open('products.csv', 'w') as f:
+with open('products.csv', 'w', encoding='utf-8') as f:
+	f.write('商品, 价格\n')
 	for p in products:
-		f.write(p[0] + ',' + p[1] + '\n')
+		f.write(p[0] + ',' + str(p[1]) + '\n')
 
